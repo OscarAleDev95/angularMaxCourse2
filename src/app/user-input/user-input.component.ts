94dbfe807e77001034c22d4b2936aabaf440c5a5
OscarAleDev95 <Oscaralejandro112@gmail.com>
2025-07-26 07:39:31 -0600

    seccion de todo lo visto primera parte

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+
+  @Output() calculate = new EventEmitter<{
+    initialInvestment : number,
+    duration          : number,
+    expectedReturn    : number,
+    annualInvestment  : number
+  }>();
+
+  enteredInitialInvestment = '0';
+  enteredAnualInvestment = '0';
+  enteredExpectedResult = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    console.log("SUBMITTED")
+  }
 
 }
