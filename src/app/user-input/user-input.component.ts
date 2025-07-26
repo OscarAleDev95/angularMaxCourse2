5ef7949325ef61985c45e80cfb94277ec6a4eb7b
OscarAlejandroCG <alejandroalejandro1102@gmail.com>
2025-07-26 17:57:12 -0600

    Comunicacion entre componentes Output de padre a hijo para imprimir los valores emitidos al enviar el formulario.
    el formulario hijo
    imprime el componente padre

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,30 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from "@angular/core";
 
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-user-input',
+  selector: "app-user-input",
   standalone: true,
   imports: [FormsModule],
-  templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  templateUrl: "./user-input.component.html",
+  styleUrl: "./user-input.component.css",
 })
 export class UserInputComponent {
-
   @Output() calculate = new EventEmitter<{
-    initialInvestment : number,
-    duration          : number,
-    expectedReturn    : number,
-    annualInvestment  : number
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
   }>();
 
-  enteredInitialInvestment = '0';
-  enteredAnualInvestment = '0';
-  enteredExpectedResult = '5';
-  enteredDuration = '10';
+  //variables declaradas para que el usuario edite
+  enteredInitialInvestment = "0";
+  enteredAnualInvestment = "0";
+  enteredExpectedResult = "5";
+  enteredDuration = "10";
 
   onSubmit() {
-    console.log("SUBMITTED")
+    console.log("SUBMITTED");
+    this.calculate.emit({
+      initialInvestment: +this.enteredAnualInvestment, //para transformar un string a un number en angular debemos de agregar solamente un signo de + antes de la variable
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedResult,
+      annualInvestment: +this.enteredAnualInvestment,
+    });
   }
-
 }
