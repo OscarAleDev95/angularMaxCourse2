645a0e432476ca9bf5e30f06e6341c824012cf02
OscarAlejandroCG <alejandroalejandro1102@gmail.com>
2025-07-31 20:19:09 -0600

    Changes for investment component

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from "@angular/core";
 
 @Component({
-  selector: 'app-investment-results',
+  selector: "app-investment-results",
   standalone: true,
   imports: [],
-  templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  templateUrl: "./investment-results.component.html",
+  styleUrl: "./investment-results.component.css",
 })
 export class InvestmentResultsComponent {
+  //**USANDO SIGNALS LO AGREGAMOS EN MINUSUCLAS EL input CÓMO UNA FUNCUIÓN */
+  // results = input<{
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }>();
 
+  @Input() results?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
 }
